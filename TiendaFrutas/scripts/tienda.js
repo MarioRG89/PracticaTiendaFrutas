56be768ff05dbe43ffed971010ed75ac0aff603a
mario <mrebollogarcia89@gmail.com>
2020-10-06 12:23:39 +0200

    [ADD] funcionalidad para sumar cada fruta al tocar imagen

---
+++ TiendaFrutas/scripts/tienda.js
@@ -0,0 +1,44 @@
+arrayFrutasContador=[0,0,0,0,0,0,0,0,0,0];
+arrayFrutasPrecio=[13.4,3.75,1.75,2.69,0.75,2.16,1.60,2.15,2.75,1.75];
+arrayFrutasNombres=["Arandanos","Fresa","ManzanaR","ManzanaV","Melon","Naranja","Pera","Platano","Sandia","Uvas"];
+
+function sumaFrutaArandano(){
+     arrayFrutasContador[0]++;
+     console.log(arrayFrutasContador);
+}
+function sumaFrutaFresa(){
+    arrayFrutasContador[1]++;
+    console.log(arrayFrutasContador);
+}
+function sumaFrutaManzanaR(){
+    arrayFrutasContador[2]++;
+    console.log(arrayFrutasContador);
+}
+function sumaFrutaManzanaV(){
+    arrayFrutasContador[3]++;
+    console.log(arrayFrutasContador);
+}
+function sumaFrutaMelon(){
+    arrayFrutasContador[4]++;
+    console.log(arrayFrutasContador);
+}
+function sumaFrutaNaranja(){
+    arrayFrutasContador[5]++;
+    console.log(arrayFrutasContador);
+}
+function sumaFrutaPera(){
+    arrayFrutasContador[6]++;
+    console.log(arrayFrutasContador);
+}
+function sumaFrutaPlatano(){
+    arrayFrutasContador[7]++;
+    console.log(arrayFrutasContador);
+}
+function sumaFrutaSandia(){
+    arrayFrutasContador[8]++;
+    console.log(arrayFrutasContador);
+}
+function sumaFrutaUva(){
+    arrayFrutasContador[9]++;
+    console.log(arrayFrutasContador);
+}
