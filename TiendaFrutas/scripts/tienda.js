a20a61f8bdc8c265b9fc2575cf9c66fdd9f90359
mario <mariorebollogarcia89@gmail.com>
2020-10-10 10:43:36 +0200

    [ADD] añadida la funcion para mostrar los resultado de frutas y kilos

--- TiendaFrutas/scripts/tienda.js
+++ TiendaFrutas/scripts/tienda.js
@@ -1,3 +1,4 @@
+//Variable globales para manejar los arrays en la diferentes funciones
 arrayFrutasContador=[0,0,0,0,0,0,0,0,0,0];
 arrayFrutasPrecio=[13.4,3.75,1.75,2.69,0.75,2.16,1.60,2.15,2.75,1.75];
 arrayFrutasNombres=["Arandanos","Fresa","ManzanaR","ManzanaV","Melon","Naranja","Pera","Platano","Sandia","Uvas"];
@@ -41,4 +42,22 @@ function sumaFrutaSandia(){
 function sumaFrutaUva(){
     arrayFrutasContador[9]++;
     console.log(arrayFrutasContador);
+}
+
+function mostrarResultados(){
+    var arrayResultado=new Array(10);
+    var resultado="";
+    
+
+    for(i=0;i<arrayResultado.length;i++){
+        arrayResultado[i]=arrayFrutasNombres[i] + " ---- " + arrayFrutasContador[i] + " kilos " ;
+    }
+    for(i=0;i<arrayResultado.length;i++){
+         resultado= resultado + arrayResultado[i] + "\n" ;
+    }
+    
+    
+    var areaTexto=document.getElementById("areaTexto");
+    var texto= document.createTextNode(resultado);
+    areaTexto.appendChild(texto);
 }
